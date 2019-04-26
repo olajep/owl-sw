c8133b7465bf597525f52f7ab0c80c08bc98df9c
Ola Jeppsson <ola.jeppsson@gmail.com>
2019-04-26 11:14:00 -0400

    .gdb: Add pretty printer for struct callstack

--- .gdbinit.py
+++ .gdbinit.py
@@ -59,10 +59,34 @@ class owl_trace_printer:
     kind = self.val["kind"]
     yield kind_to_field(kind), self.val[kind_to_field(kind)]
 
+class callstack_printer:
+  """Print a callstack struct."""
+
+  def __init__(self, val):
+      self.val = val
+
+  def to_string(self):
+    return None
+
+  def children(self):
+    for field in self.val.type.fields():
+      key = field.name
+      val = self.val[key]
+      if key == "frames":
+        if not val: yield key, "NULL"
+        else:
+          yield key + "[0]", (val+0).dereference()
+          yield key + "[1]", (val+1).dereference()
+          yield key + "[2]", (val+2).dereference()
+      else:
+        yield key, val
+
+
 def build_pretty_printer():
   pp = gdb.printing.RegexpCollectionPrettyPrinter("owl")
   pp.add_printer('owl_trace', '^owl_trace$', owl_trace_printer)
   pp.add_printer('owl_.*_trace', '^owl_.*_trace$', owl_trace_field_printer)
+  pp.add_printer('callstack', '^callstack$', callstack_printer)
   return pp
 
 gdb.printing.register_pretty_printer(
