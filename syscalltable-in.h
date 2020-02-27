b3d52ca1f204b85066e025f6b7e10194aa5338b5
Ola Jeppsson <ola.jeppsson@gmail.com>
2020-02-27 11:09:04 -0500

    syscalltable.h: Define enum tgt_syscall_t

--- syscalltable-in.h
+++ syscalltable-in.h
@@ -4,8 +4,14 @@ static const char *syscalltable[] = {
 #endif
 #define __SYSCALL(x, y) [x] = #y,
 #include "asm/unistd.h"
+};
+
+enum tgt_syscall_t {
+#undef __SYSCALL
+#define __SYSCALL(x, y) tgt_##y = x,
+#include "asm/unistd.h"
+};
 #ifdef ___SYSCALL_TMP
 #define __SYSCALL ___SYSCALL_TMP
 #undef ___SYSCALL_TMP
 #endif
-};
